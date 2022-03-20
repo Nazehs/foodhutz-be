5d034e63842a3004927928a3b118dbe9671c1ed8
Nazeh Abel <talentednazeh@gmail.com>
2022-03-20 18:43:44 +0000

    rename folder

--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,21 +1,4 @@
 const { GraphQLScalarType, Kind } = require("graphql");
-
-const dateScalar = new GraphQLScalarType({
-  name: "Date",
-  description: "Date custom scalar type",
-  serialize(value) {
-    return value.getTime(); // Convert outgoing Date to integer for JSON
-  },
-  parseValue(value) {
-    return new Date(value); // Convert incoming integer to Date
-  },
-  parseLiteral(ast) {
-    if (ast.kind === Kind.INT) {
-      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
-    }
-    return null; // Invalid hard-coded value (not an integer)
-  },
-});
 // create resolvers
 const login = require("./user/login");
 const signup = require("./user/signup");
@@ -64,6 +47,23 @@ const updateOrder = require("./order/updateOrder");
 const updateRestaurant = require("./restaurant/updateRestaurant");
 const updateBankDetails = require("./bankDetails/updateBankDetails");
 const updateMenu = require("./menu/updateMenu");
+
+const dateScalar = new GraphQLScalarType({
+  name: "Date",
+  description: "Date custom scalar type",
+  serialize(value) {
+    return value.getTime(); // Convert outgoing Date to integer for JSON
+  },
+  parseValue(value) {
+    return new Date(value); // Convert incoming integer to Date
+  },
+  parseLiteral(ast) {
+    if (ast.kind === Kind.INT) {
+      return new Date(parseInt(ast.value, 10)); // Convert hard-coded AST string to integer and then to Date
+    }
+    return null; // Invalid hard-coded value (not an integer)
+  },
+});
 const resolvers = {
   Date: dateScalar,
   Query: {
