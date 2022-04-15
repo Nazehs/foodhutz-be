9173aac0c73f9812af4f3c9ceb16bf5de655752c
Nazeh Abel <talentednazeh@gmail.com>
2022-04-15 20:53:53 +0100

    new changes added for restaurant

--- src/resolvers/storeOwner/createStoreOwner.js
+++ src/resolvers/storeOwner/createStoreOwner.js
@@ -1,14 +1,22 @@
 const { ApolloError, AuthenticationError } = require("apollo-server-express");
-const { StoreOwner } = require("../../models");
+const { StoreOwner, Driver, User } = require("../../models");
 const { signToken } = require("../../utils/auth");
 
 const createStoreOwner = async (_, { input }) => {
   try {
-    const user = await await StoreOwner.create(input);
-    return {
-      token: signToken(user),
-      user,
-    };
+    const isUserExisting =
+      (await Driver.findOne({ email: input.email })) ||
+      (await User.findOne({ email: input.email })) ||
+      (await StoreOwner.findOne({ email: input.email }));
+    if (!isUserExisting) {
+      await sentSMS(input.phoneNumber);
+      const user = await await StoreOwner.create(input);
+      return {
+        token: signToken(user),
+        user,
+      };
+    }
+    throw new ApolloError("Failed to sign up. user already exist");
   } catch (error) {
     console.log(`[ERROR]: Failed to create StoreOwner | ${error.message}`);
     throw new ApolloError("Failed to create StoreOwner");
