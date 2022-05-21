847efda4d3253c448268c01d4dd9396d8536c76d
Nazeh Abel <talentednazeh@gmail.com>
2022-05-21 20:51:25 +0100

    bug hunting

--- src/resolvers/user/signup.js
+++ src/resolvers/user/signup.js
@@ -10,16 +10,16 @@ const signup = async (_, { input }) => {
     console.log("%%%% input %%%% ", input);
     const isUserExisting = await User.findOne({ email: input.email });
     console.log("%%% isUserExisting %% ", isUserExisting);
-    if (!isUserExisting) {
-      const user = await User.create(input);
-      console.log("created user ::::: ", user);
-      return {
-        token: signToken(user),
-        user,
-      };
-    } else {
-      throw new ApolloError("Failed to sign up user already exist");
-    }
+    // if (!isUserExisting) {
+    //   const user = await User.create(input);
+    //   console.log("created user ::::: ", user);
+    //   return {
+    //     token: signToken(user),
+    //     user,
+    //   };
+    // } else {
+    //   throw new ApolloError("Failed to sign up user already exist");
+    // }
   } catch (error) {
     console.log(`[ERROR]: Failed to sign up | ${error.message}`);
     throw new ApolloError(`Failed to sign up || ${error.message}`);
