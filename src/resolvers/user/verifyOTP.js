a85d7da2605ed6b59f7002c20108a7e353139e94
Nazeh Abel <talentednazeh@gmail.com>
2022-05-21 21:32:30 +0100

    bug hunting useless and funny bug

--- src/resolvers/user/verifyOTP.js
+++ src/resolvers/user/verifyOTP.js
@@ -6,17 +6,15 @@ const { verifySMS } = require("../../utils/sms");
 const verifyOTP = async (_, { input }) => {
   try {
     // verify the otp
-    await verifySMS({
+    const response = await verifySMS({
       to: input.phoneNumber,
       code: input.code,
     });
-    // if (response && response.status == "approved" && response.valid) {
-    //   await User.findOneAndUpdate({
-    //     email: input.email,
-    //     $set: { status: true },
-    //   });
-    // }
-    return { status: 0, message: "Approved", isValid: true };
+    if (response && response.status == "approved" && response.valid) {
+      return { status: 0, message: "Approved", isValid: true };
+    } else {
+      return { status: 1, message: "Wrong", isValid: false };
+    }
   } catch (error) {
     console.log(`[ERROR]: Failed to update OTP  details | ${error.message}`);
     throw new ApolloError("Failed to update OTP details");
